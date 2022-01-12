a014f12173ca059875a1113fcbe20b4d44d1fb51
wjr <1955610329@qq.com>
2022-01-12 19:54:12 +0800

    branchTest second commit

--- src/main/java/com/wjr/git.java
+++ src/main/java/com/wjr/git.java
@@ -6,5 +6,6 @@ public class git {
         System.out.println("hello git again");
         System.out.println("hello git again");
         System.out.println("hello git again");
+        System.out.println("hello git again branchTest");
     }
 }
