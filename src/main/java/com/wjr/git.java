f8544487f0e05115946835b1ab82f778bfa7dfff
XiaoShifuMeatEater <97449487+XiaoShifuMeatEater@users.noreply.github.com>
2022-01-14 19:28:17 +0800

    Update git.java

--- src/main/java/com/wjr/git.java
+++ src/main/java/com/wjr/git.java
@@ -10,5 +10,6 @@ public class git {
         System.out.println("hello git again branchTest");
         System.out.println("push test");
         System.out.println("push test1");
+        System.out.println("pull test");
     }
 }
