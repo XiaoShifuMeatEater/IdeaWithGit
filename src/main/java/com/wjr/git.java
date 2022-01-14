5ce4252d1bac949859252f41b0fb10ad0985fda0
wjr <1955610329@qq.com>
2022-01-14 19:26:31 +0800

    master push test3

--- src/main/java/com/wjr/git.java
+++ src/main/java/com/wjr/git.java
@@ -9,5 +9,6 @@ public class git {
         System.out.println("hello git again master");
         System.out.println("hello git again branchTest");
         System.out.println("push test");
+        System.out.println("push test1");
     }
 }
